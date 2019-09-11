e7dedca35eb2a7f931b893a03d00150dfc391069
MaKyuSeok <mks502@naver.com>
2019-09-11 20:13:51 +0900

    프로그래머스 level2 스킬트리 - 개선
    프로그래머스 level2 스킬트리 알고리즘 풀이를
    기존 ArrayList에 담아서 contains를 사용하던 것을
    String 의 contains 를 사용해서
    더 코드를 간결하게 하는 방법으로 개선추가

--- src/level2/스킬트리/Solution.java
+++ src/level2/스킬트리/Solution.java
@@ -28,4 +28,30 @@ class Solution {
 		}
 		return true;
 	}
+
+	//// 2. ArrayList의 contains -> String 의 contains를 사용해서 ArrayList를 사용하지 않는 방법
+
+	public int solution2(String skill, String[] skill_trees) {
+		int answer = 0;
+		for (String skillTree : skill_trees) {
+			if (isAvailable2(skill, skillTree))
+				answer++;
+		}
+		return answer;
+	}
+
+	public boolean isAvailable2(String skill, String skillTree) {
+		int order = 0; // 현재 배운 선행 스킬 순서
+		for (int i = 0; i < skillTree.length(); i++) {
+			char selectedSkill = skillTree.charAt(i); // 찍어야 할 스킬
+			if (skill.contains(String.valueOf(selectedSkill))) { // 선행스킬 목록에 있으면 선행스킬 확인 필요
+				if (selectedSkill == (char) skill.charAt(order))
+					order++; // 배울 수 있음
+				else
+					return false; // 배울 수 없으면 더이상 확인 필요x false 반환
+			}
+		}
+		return true;
+	}
+
 }
