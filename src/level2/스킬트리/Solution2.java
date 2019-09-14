905f075428e453a7031385836efbf775783882ac
MaKyuSeok <mks502@naver.com>
2019-09-14 17:05:12 +0900

    프로그래머스 level2 스킬트리 Refactoring
    프로그래머스 level2 스킬트리 Refactoring

--- src/level2/스킬트리/Solution2.java
+++ src/level2/스킬트리/Solution2.java
@@ -1,26 +1,23 @@
 package level2.스킬트리;
 
-import java.util.*;
+public class Solution2 {
+////2. ArrayList의 contains -> String 의 contains를 사용해서 ArrayList를 사용하지 않는 방법
 
-class Solution {
 	public int solution(String skill, String[] skill_trees) {
 		int answer = 0;
-		ArrayList<Character> skillList = new ArrayList<Character>();
-		for (int i = 0; i < skill.length(); i++)
-			skillList.add(skill.charAt(i));
 		for (String skillTree : skill_trees) {
-			if (isAvailable(skillList, skillTree))
+			if (isAvailable(skill, skillTree))
 				answer++;
 		}
 		return answer;
 	}
 
-	private boolean isAvailable(ArrayList skillList, String skillTree) {
+	private boolean isAvailable(String skill, String skillTree) {
 		int order = 0; // 현재 배운 선행 스킬 순서
 		for (int i = 0; i < skillTree.length(); i++) {
 			char selectedSkill = skillTree.charAt(i); // 찍어야 할 스킬
-			if (skillList.contains(selectedSkill)) { // 선행스킬 목록에 있으면 선행스킬 확인 필요
-				if (selectedSkill == (char) skillList.get(order))
+			if (skill.contains(String.valueOf(selectedSkill))) { // 선행스킬 목록에 있으면 선행스킬 확인 필요
+				if (selectedSkill == (char) skill.charAt(order))
 					order++; // 배울 수 있음
 				else
 					return false; // 배울 수 없으면 더이상 확인 필요x false 반환
