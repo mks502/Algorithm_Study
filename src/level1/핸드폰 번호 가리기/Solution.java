c276a1ba7f862d526c177293638be2b3c3e0f977
MaKyuSeok <mks502@naver.com>
2020-01-29 20:27:14 +0900

    Programmers level1 핸드폰 번호 가리기
    Programmers level1 핸드폰 번호 가리기

---
+++ src/level1/핸드폰 번호 가리기/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String phone_number) {
+        StringBuilder answer = new StringBuilder("");
+        for(int i=0; i<phone_number.length(); i++){
+            if(i >= phone_number.length()-4)
+                answer.append(phone_number.charAt(i));
+            else
+                answer.append("*");
+        }
+        return answer.toString();
+    }
+}
